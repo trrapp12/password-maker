358d830105b3a47aeda8e1ea3191d39250803068
Trevor Rapp <11747875+trrapp12@users.noreply.github.com>
2024-08-09 03:38:25 -0600

    fixes it so that the slider works, the displays now display on the first four clicks, the pseudo element displaying the slider number nicely tracks above the slider and it is generally responsive

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 (() => {
-
   // grab the necessary DOM elements
   const passwordButton = document.getElementById('button');
   const outPut1 = document.getElementById('option-1');
   const outPut2 = document.getElementById('option-2');
   const outPut3 = document.getElementById('option-3');
   const outPut4 = document.getElementById('option-4');
-  const textInput = document.getElementById('text');
+  // const textInput = document.getElementById('text');
+  const rangeSlider = document.getElementById('text');
+  const sliderValue = document.getElementById('sliderValue');
 
+  console.log('outPut1 is', outPut1);
+  console.log(
+    'rangeSlider is',
+    rangeSlider,
+    'rangeSlider.value is',
+    rangeSlider.value,
+    'typeof rangeSlider.value is',
+    typeof rangeSlider.value
+  );
   // create variables
 
   let passwordArray1 = [];
@@ -15,93 +25,37 @@
   let passwordArray3 = [];
   let passwordArray4 = [];
 
-
-  let characterArray = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-~'.split('');
+  let characterArray =
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-~'.split(
+      ''
+    );
 
   // create functions
 
-  // section 1 - copy functionality
+  function initializeSlider() {
+    console.log('initializeSlider called');
+    const value = rangeSlider.value || '';
+    // since the left position of the thumb changes according to percentage, we can use the percentage to decide where the slider will be
+    const min = parseInt(rangeSlider.min, 10);
+    const max = parseInt(rangeSlider.max, 10);
+    const percentage = ((value - min) / (value - max)) * 100;
 
-  function findTarget (evt) {
-    if (evt.target.className === 'option') {
-      
-      targetId = evt.target.id;
-      targetElement = document.getElementById(targetId);
-      if (evt.target.innerHTML) {
-        navigator.clipboard.writeText(evt.target.value)
-        alert("Password successfully copied!")
-      } else {
-        alert('no password to copy')
-      }
-    }
+    sliderValue.innerText = value;
+
+    // calculate left position
+    let originalBoundaries = rangeSlider.getBoundingClientRect();
+    let originalWidth = originalBoundaries.width;
+    let originalLeft = originalBoundaries.left;
+
+    let numAfterPerc = value / 50;
+    sliderValue.style.left = `calc(${
+      originalLeft + originalWidth * numAfterPerc
+    }px + ${numAfterPerc * 0.5}rem)`; // Adjust formula as needed
+    sliderValue.style.top = `${originalBoundaries.top - 50}px`;
   }
 
-  window.addEventListener('click', findTarget)
-  // section 2 - password generation
-  function getNumber() {
-    let newNumber = document.getElementById('text').value
-    return (newNumber === "" ? 8: Math.floor(newNumber));   
-  }
-
-  function validateNumber (newNumber) {
-    if (!newNumber) {
-      alert('please enter a non-zero number')
-    } else if (Math.sign(newNumber) !== 1){
-      alert('please enter a positive number')
-    } else {
-      return newNumber;
-    }
-  }
-
-  function generateRandomNumber() {
-    return (Math.floor(Math.random() * 77) + 1)
-  }
-
-  function updateArray(chosenarray, value) {
-    chosenarray.push(characterArray[value])
-  }
-
-  function displayArray (outPutArray) {
-
-    let outPutRegex = outPutArray.toString().replace(/,/g, '');
-
-    if (outPut1.textContent === '' || outPut1.textContent === 'password option 1') {
-      outPut1.innerText = outPutRegex;
-    } else if (outPut2.textContent === '' || outPut2.textContent === 'password option 2') {
-      outPut2.innerText = outPutRegex;
-    } else if (outPut3.textContent === '' || outPut3.textContent === 'password option 3') {
-      outPut3.innerText = outPutRegex;
-    } else if (outPut4.textContent === '' || outPut4.textContent === 'password option 4') {
-      outPut4.innerText = outPutRegex;
-    } 
-  }
-
-  function reset () {
-    passwordArray1 = [];
-    passwordArray2 = [];
-    passwordArray3 = [];
-    passwordArray4 = [];
-    outPut1.innerText = outPut2.innerText = outPut3.innerText = outPut4.innerText = '';
-    textInput.value = '';
-    passwordButton.innerText = "Generate Password"
-  }
-
-  function checkWhichSlotsAreEmpty () {
-    if (passwordArray1.length === 0) {
-      return passwordArray1
-    } else if (passwordArray2.length === 0) {
-      return passwordArray2
-    } else if (passwordArray3.length === 0) {
-      return passwordArray3
-    } else if (passwordArray4.length === 0) {
-      passwordButton.innerText = "Reset";
-      return passwordArray4
-    } else {
-      reset()
-    }
-  }
-
-  function generateRandomString () {
+  function generateRandomString() {
+    console.log('generateRandomString called');
     let num = getNumber();
     validateNumber(num);
     var x = checkWhichSlotsAreEmpty();
@@ -109,10 +63,124 @@
       var y = generateRandomNumber();
       updateArray(x, y);
     }
+    console.log('x going into displayArray is', x);
     displayArray(x);
   }
 
-  // add listeners
-  passwordButton.addEventListener('click', generateRandomString)
+  function getNumber() {
+    console.log('getNumber called');
+    let newNumber = document.getElementById('text').value;
+    console.log('newNumber is', newNumber);
+    return newNumber === '' ? 8 : parseInt(newNumber, 10);
+  }
 
-})()
+  function validateNumber(newNumber) {
+    console.log('validateNumber called: newNumber is', newNumber);
+    if (!newNumber) {
+      alert('please enter a non-zero number');
+    } else if (Math.sign(newNumber) !== 1) {
+      alert('please enter a positive number');
+    } else {
+      return newNumber;
+    }
+  }
+
+  function updateArray(chosenarray, value) {
+    console.log(
+      'updateArray called.  chosenarray input parameter is',
+      chosenarray,
+      'value is',
+      value
+    );
+    chosenarray.push(characterArray[value]);
+    console.log('chosenarray was updated to', chosenarray);
+  }
+
+  function generateRandomNumber() {
+    console.log('generateRandomNumber called');
+    return Math.floor(Math.random() * characterArray.length);
+  }
+
+  function checkWhichSlotsAreEmpty() {
+    console.log('checkWhichSlotsAreEmpty called');
+    if (passwordArray1.length === 0) {
+      return passwordArray1;
+    } else if (passwordArray2.length === 0) {
+      return passwordArray2;
+    } else if (passwordArray3.length === 0) {
+      return passwordArray3;
+    } else if (passwordArray4.length === 0) {
+      passwordButton.innerText = 'Reset';
+      return passwordArray4;
+    } else {
+      reset();
+    }
+  }
+
+  function findTarget(evt) {
+    console.log('findTarget fired');
+    let targetElement;
+    if (evt.target.className === 'option') {
+      targetId = evt.target.id;
+      targetElement = document.getElementById(targetId);
+      if (evt.target.innerHTML) {
+        navigator.clipboard.writeText(evt.target.value);
+        alert('Password successfully copied!');
+      } else {
+        alert('no password to copy');
+      }
+    }
+  }
+
+  function displayArray(outPutArray) {
+    console.log('displayArray called');
+    let outPutRegex = outPutArray.toString().replace(/,/g, '');
+    console.log('outPutRegex is', outPutRegex);
+    if (outPut1.innerText === '' || outPut1.innerText === 'password option 1') {
+      console.log('outPut1 is empty');
+      outPut1.innerText = outPutRegex;
+    } else if (
+      outPut2.innerText === '' ||
+      outPut2.innerText === 'password option 2'
+    ) {
+      console.log('outPut2 is empty');
+      outPut2.innerText = outPutRegex;
+    } else if (
+      outPut3.innerText === '' ||
+      outPut3.innerText === 'password option 3'
+    ) {
+      console.log(' outPut3 is empty');
+      outPut3.innerText = outPutRegex;
+    } else if (
+      outPut4.innerText === '' ||
+      outPut4.innerText === 'password option 4'
+    ) {
+      console.log('outPut4 is empty');
+      outPut4.innerText = outPutRegex;
+    }
+  }
+
+  function reset() {
+    console.log('reset called');
+    passwordArray1 = [];
+    passwordArray2 = [];
+    passwordArray3 = [];
+    passwordArray4 = [];
+    outPut1.innerText =
+      outPut2.innerText =
+      outPut3.innerText =
+      outPut4.innerText =
+        '';
+    rangeSlider.value = '';
+    passwordButton.innerText = 'Generate Password';
+  }
+
+  // add listeners
+  passwordButton.addEventListener('click', generateRandomString);
+
+  // adjustments to turn text field into range finder
+  document.addEventListener('click', findTarget);
+  document.addEventListener('DOMContentLoaded', initializeSlider);
+  window.addEventListener('resize', initializeSlider);
+  rangeSlider.addEventListener('input', initializeSlider);
+})();
